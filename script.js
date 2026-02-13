ffdbf649521e98682506942c319aa80b225cbadc
Abdutolib Juraqulov <juraqulovabdutolib1@gmail.com>
2026-02-13 09:38:28 +0100

    Pirat smooth laufen lassen

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let frame = 0;
 let state = 'IDLE';
 let left = 300;
+let leftArrow = false;
+let rightArrow = false;
 
 setInterval(moveCharacter, 75);
-setInterval(updateBackground, 13);
+setInterval(updateGame, 13);
+
 document.onkeydown = checkKey;
+document.onkeyup = unCheckKey;
 
 function checkKey(e) {
 
@@ -12,23 +16,54 @@ function checkKey(e) {
 
     if (e.keyCode == '37') {
        // left arrow
+       leftArrow = true;
        left -= 5;
        setState('WALK');
     }
     else if (e.keyCode == '39') {
        // right arrow
+       rightArrow = true;
        left += 5;
        setState('WALK');
     }
 }
 
-function updateBackground(){
+function unCheckKey(e) {
+    e = e || window.event;
+
+    if (e.keyCode == '37') {
+        leftArrow = false;
+    } else if (e.keyCode == '39') {
+        rightArrow = false;
+    }
+}
+
+
+function updateGame(){
     currentBackground.style.objectPosition = `${-left}px`;
+    if(leftArrow){
+        left -= 5;
+    }
+    if(rightArrow){
+        left += 5;
+    }
+
+    if(leftArrow || rightArrow){
+        setState('WALK')
+    } else {
+        setState('IDLE')
+    }
 }
 
 function moveCharacter(){
     pirate.src = `img/PNG/2/2_entity_000_${state}_00${frame}.png`;
     frame++;
+    if(leftArrow){
+        pirate.style.transform = "scaleX(-1)";
+    }
+    if(rightArrow){
+        pirate.style.transform = "scaleX(1)";
+    }
 
     if(frame == 7){
         frame = 0;
